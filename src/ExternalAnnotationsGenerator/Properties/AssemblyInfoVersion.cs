95ff2398945986afadaad319f62583613251db30
Julien Roncaglia <julien@roncaglia.fr>
2015-09-17 15:42:47 +0200

    Don't save packages folder in AppVeyor

--- src/ExternalAnnotationsGenerator/Properties/AssemblyInfoVersion.cs
+++ src/ExternalAnnotationsGenerator/Properties/AssemblyInfoVersion.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("ExternalAnnotationsGenerator")]
 [assembly: AssemblyProductAttribute("ExternalAnnotationsGenerator")]
 [assembly: AssemblyDescriptionAttribute("Fluent Library for generating ReSharper External annotation files")]
-[assembly: AssemblyVersionAttribute("1.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: AssemblyVersionAttribute("1.1.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.1.0.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0.0.0";
+        internal const string Version = "1.1.0.0";
     }
 }
