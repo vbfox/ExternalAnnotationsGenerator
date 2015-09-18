13f8844f18842866881a92ae9daae1a71773d8c9
Julien Roncaglia <julien@roncaglia.fr>
2015-09-18 15:19:17 +0200

    Add marker types for class and struct
    As specified in #3

---
+++ src/ExternalAnnotationsGenerator/T.cs
@@ -0,0 +1,9 @@
+﻿namespace ExternalAnnotationsGenerator
+{
+    /// <summary>
+    /// Type used as a replacement for a real type in methods requiring any type arguments or specifically a class.
+    /// </summary>
+    public class T
+    {
+    }
+}
---
+++ src/ExternalAnnotationsGenerator/TStruct.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ExternalAnnotationsGenerator
+{
+    /// <summary>
+    /// Type used as a replacement for a real type in methods requiring specifically a struct.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public struct TStruct
+    {
+    }
+}
